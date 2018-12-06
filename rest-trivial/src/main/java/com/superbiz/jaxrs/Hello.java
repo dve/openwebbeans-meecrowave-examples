0f67747758053a6f8ff807e684c3e3e45372d10c
Daniel Nordhoff-Vergien <dve@vergien.net>
2018-12-06 16:17:14 +0100

    Use jax-rs annotation on interface

---
+++ rest-trivial/src/main/java/com/superbiz/jaxrs/Hello.java
@@ -0,0 +1,15 @@
+package com.superbiz.jaxrs;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+@Path("hello")
+public interface Hello {
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  String sayHi();
+
+}
--- rest-trivial/src/main/java/com/superbiz/jaxrs/HelloEndpoint.java
+++ rest-trivial/src/main/java/com/superbiz/jaxrs/HelloEndpoint.java
@@ -19,17 +19,13 @@
 package com.superbiz.jaxrs;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
-@Path("hello")
+
 @ApplicationScoped
-public class HelloEndpoint {
+public class HelloEndpoint implements Hello {
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
+   
+    @Override
     public String sayHi() {
         return "Hello World";
     }
